5d9f0810d0dfddbd6dd9098ce06d16739337be54
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-07 21:16:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,10 @@
 def oxford_comma(array)
-  
- while array[
-     array.join(", ")
-  end
-    array.join(", and ")
-end
+case array.length 
+  when 1
+  "#{array[0]}"
+when 2
+  array[0..1].join(" and ")
+else 
+  array[0...-1].join(", ") << ", and #{array[-1]}"
+end 
+end  
